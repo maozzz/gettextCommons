dc1a6d8ebe3a2082c55465d9150c4fa66a9dce42
maoz <mao_z@mail.ru>
2019-01-14 16:43:12 +0300

    problems with plurable

--- src/main/java/ru/villex/gettext/plurals/ExpressionPlurable.java
+++ src/main/java/ru/villex/gettext/plurals/ExpressionPlurable.java
@@ -8,8 +8,6 @@ import java.io.FileWriter;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -43,60 +41,50 @@ public class ExpressionPlurable implements Plurable {
 
 
     public static Plurable generatePlurableClass(String expression) {
-        try {
-            ClassLoader.getSystemClassLoader().loadClass("ru.villex.gettext.plurals.Plurable");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            String className = "Plurable";
-            // create an empty source file
-            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Creating class for plurable");
-            File sourceFile = File.createTempFile(className, ".java");
-            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Creating class for plurable");
-            sourceFile.deleteOnExit();
-
-            // generate the source code, using the source filename as the class name
-            className = sourceFile.getName().split("\\.")[0];
-            String sourceCode = " public class " + className + " implements " + Plurable.class.getName() + " {"
-                    + " public int pluralEval(long n) {"
-                    + " Object o = " + expression + (expression.endsWith(";") ? "" : ";")
-                    + " if (o instanceof Boolean) { return ((Boolean) o) ? 1 : 0;}else { return (Integer) o;} "
-                    + " }"
-                    + "} ";
-
-            FileWriter writer = new FileWriter(sourceFile);
-            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Writing class for plurable");
-            writer.write(sourceCode);
-            writer.close();
-            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Writed! Compile...");
-
-            // compile the source file
-            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-            StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-            File parentDirectory = sourceFile.getParentFile();
-            fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(parentDirectory));
-            Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(sourceFile));
-            compiler.getTask(null, fileManager, null, null, null, compilationUnits).call();
-            fileManager.close();
-            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Loading compiled class.");
-
-            // load the compiled class
-            URL[] urLs = ((URLClassLoader) (Thread.currentThread().getContextClassLoader())).getURLs();
-            List<URL> urls = Arrays.asList(urLs);
-            urls.add(parentDirectory.toURI().toURL());
-            URL[] urls1 = urls.stream().toArray(URL[]::new);
-            URLClassLoader classLoader = URLClassLoader.newInstance(urls1, ClassLoader.getSystemClassLoader());
-            Class<?> plurable = classLoader.loadClass(className);
-
-            // return instance
-            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Plurable ready");
-            return (Plurable) plurable.newInstance();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+//        try {
+//            String className = "Plurable";
+//            // create an empty source file
+//            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Creating class for plurable");
+//            File sourceFile = File.createTempFile(className, ".java");
+//            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Creating class for plurable");
+//            sourceFile.deleteOnExit();
+//
+//            // generate the source code, using the source filename as the class name
+//            className = sourceFile.getName().split("\\.")[0];
+//            String sourceCode = " public class " + className + " implements " + Plurable.class.getName() + " {"
+//                    + " public int pluralEval(long n) {"
+//                    + " Object o = " + expression + (expression.endsWith(";") ? "" : ";")
+//                    + " if (o instanceof Boolean) { return ((Boolean) o) ? 1 : 0;}else { return (Integer) o;} "
+//                    + " }"
+//                    + "} ";
+//
+//            FileWriter writer = new FileWriter(sourceFile);
+//            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Writing class for plurable");
+//            writer.write(sourceCode);
+//            writer.close();
+//            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Writed! Compile...");
+//
+//            // compile the source file
+//            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+//            StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+//            File parentDirectory = sourceFile.getParentFile();
+//            fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(parentDirectory));
+//            Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(sourceFile));
+//            compiler.getTask(null, fileManager, null, null, null, compilationUnits).call();
+//            fileManager.close();
+//            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Loading compiled class.");
+//
+//            // load the compiled class
+//            URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{parentDirectory.toURI().toURL()}, ClassLoader.getSystemClassLoader());
+//            Class<?> plurable = classLoader.loadClass(className);
+//
+//            // return instance
+//            Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Plurable ready");
+//            return (Plurable) plurable.newInstance();
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
             Logger.getLogger(ExpressionPlurable.class.getSimpleName()).severe("Creating plurable for expr: " + expression);
             return new ExpressionPlurable(expression);
-        }
+//        }
     }
 }
